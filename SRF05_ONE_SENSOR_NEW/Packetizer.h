f73af1b409ea78dc578a20bf61b107467510ad28
franeum desk <elettrochuck@gmail.com>
2021-05-25 19:31:20 +0200

    exps

---
+++ SRF05_ONE_SENSOR_NEW/Packetizer.h
@@ -0,0 +1,19 @@
+
+#ifndef Packetizer_h
+#define Packetizer_h
+
+#include <Arduino.h>
+
+class Packetizer
+{
+public:
+  Packetizer(byte addr);
+  byte *packetize(int value);
+  byte SIZE;
+
+private:
+  byte _addr;
+  byte message[3];
+};
+
+#endif Packetizer_h
--- SRF05_ONE_SENSOR_PUSH/SRF05_ONE_SENSOR_PUSH.ino
+++ SRF05_ONE_SENSOR_PUSH/SRF05_ONE_SENSOR_PUSH.ino
@@ -9,56 +9,47 @@ HCSR04      hc(TRIG_PIN, ECHO_PIN_1); // first argument is trig pin, second is t
 Packetizer  pack1(0);                 // arg: address of the sensor (0-127), use it to labeling sensors, 0 is the first, 1 is the second, and so...
 
 int         values[NR_OF_SAMPLES] = { 0 };
-byte        count  = 0;
-int         prev = 0;
+byte        count     = 0;
+int         prev      = 0;
 int         max_value = 0;
 int         min_value = 0;
+int         threshold = 50;
+int         state     = 0;
+
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  values[count] = hc.dist();
-
+  //values[count] = hc.dist();
+  int value = hc.dist();
+  
   // pack1.packetize(value) return a byte pointer
   //Serial.write(pack1.packetize(value), pack1.SIZE);  
   //if ((value < 4) | (value < 500))
     //Serial.write(pack1.packetize(value), pack1.SIZE);
 
-  int somma = 0;
-  
-  for (int i=0; i<NR_OF_SAMPLES; i++) {
-    somma += values[i];
-  }
 
   /*
-  for (int i = 0; i < NR_OF_SAMPLES; i++) {
-    if (values[i] > max_value) max_value = values[i];
-    if (values[i] < min_value) min_value = values[i];
-  }
-
-  somma = somma - max_value - min_value;
-*/
-  int median = int(somma / (float)NR_OF_SAMPLES);
-
-
-  
   if (median != prev) {
     Serial.println(median);
     prev = median;
   }
+  */
   
   //Serial.println(median);
 
-/*
-  int value = hc.dist();
-  if (value < 600)
-    Serial.println(value);
-  */
-  count++;
-  if (count >= NR_OF_SAMPLES) count = 0;
+  if (value < threshold) {
+    if (prev == 0) {
+      state = !state;       
+      prev = 1; 
+      Serial.println(state);
+    }
+  } else {
+    prev = 0;  
+  }
   
-  delay(1);
+  delay(5);
   
 }
