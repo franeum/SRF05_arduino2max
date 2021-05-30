aef94cf33502826b48c9ef63fd488770c731c8b0
franeum <elettrochuck@gmail.com>
2021-05-30 15:44:31 +0200

    fix

---
+++ SRF05_ONE_SENSOR_DEF/SRF05_ONE_SENSOR_DEF.ino
@@ -0,0 +1,28 @@
+#include <HCSR04.h>
+#include "Packetizer.h"
+
+#define NR_OF_SAMPLES 16
+#define TRIG_PIN      5
+#define ECHO_PIN_1    6
+
+HCSR04      hc(TRIG_PIN, ECHO_PIN_1); // first argument is trig pin, second is the echo
+Packetizer  pack1(0);                 // arg: address of the sensor (0-127), use it to labeling sensors, 0 is the first, 1 is the second, and so...
+
+int         values[NR_OF_SAMPLES] = { 0 };
+byte        count  = 0;
+int         prev = 0;
+int         max_value = 0;
+int         min_value = 0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  //values[count] = hc.dist();
+  
+  Serial.println((int)hc.dist());
+  
+  delay(50);
+  
+}
